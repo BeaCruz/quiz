0fb77c6a14ad87d80d95a90a43b1314e0e85db3f
BeaCruz <cursosmiriada@gmail.com>
2015-06-29 20:41:07 +0200

    Primera página y favicon
    Primera página y favicon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  //, user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -15,7 +15,9 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+//app.use(express.favicon());
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.favicon(__dirname + '/public/favicon_2.ico'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -28,7 +30,33 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/users', user.list);
+
+app.use(function (req,res,next){
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
